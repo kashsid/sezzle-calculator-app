49ececb8d294fb274eec9175eade0f8a177d827e
kashsid <kashifsiddique@gmail.com>
2019-06-09 23:39:31 -0500

    fixes the error and implements expression validation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { calculate } from './redux/Reducers/Calculate'
-import Calculator from './Components/Calculator'
-import * as fromCalculator from './redux/Reducers'
-import './App.css'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  calculate,
+  deleteLastEntry,
+  clear,
+  evaluateExpression
+} from "./redux/Reducers/Calculate";
+import Calculator from "./Components/Calculator";
+import * as fromCalculator from "./redux/Reducers";
+import "./App.css";
 
 export class App extends Component {
-
   componentDidMount() {
     console.log("mounted calculator!");
   }
 
   render() {
     return (
-      <div className='calculator--container'>
+      <div className="calculator--container">
         <Calculator.Screen {...this.props} />
         <Calculator.Keypad {...this.props} />
       </div>
@@ -21,24 +25,31 @@ export class App extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     expression: fromCalculator.getExpression(state),
     total: fromCalculator.getTotal(state)
-  }
-}
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    calculate: (buttonKey) => {
-      dispatch(calculate(buttonKey))
+    calculate: buttonKey => {
+      dispatch(calculate(buttonKey));
     },
-  }
-}
-
+    delete: () => {
+      dispatch(deleteLastEntry());
+    },
+    clear: () => {
+      dispatch(clear());
+    },
+    evaluate: () => {
+      dispatch(evaluateExpression());
+    }
+  };
+};
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
+  mapDispatchToProps
 )(App);
-
--- src/redux/Reducers/calculateReducer.js
+++ src/redux/Reducers/calculateReducer.js
@@ -4,19 +4,74 @@ import calculate from '../../Components/Calculate'
 // Making reference to a variable 
 //types.SET_EXPRESSION instead of hard coding the action type
 let initialState = {
-    expression: '',
+    expression: "",
     total: 0
+};
+
+function setExpression({ expression, total }, action) {
+    if (/[\d]*[-+%*/.]$/.exec(expression) && /[-+%*/.]/.exec(action.payload)) {
+        console.log("b", expression);
+        expression = expression.slice(0, expression.length - 1);
+        console.log("a", expression);
+    }
+
+    switch (action.type) {
+        case types.SET_EXPRESSION:
+            if (["+", "/", "*", "%"].includes(action.payload) && !expression) {
+                return `${total}${action.payload}`;
+            }
+            return `${!expression && total ? total : ""}${expression +
+                action.payload}`;
+        default:
+            return expression;
+    }
 }
+
 export default (state = initialState, action) => {
     switch (action.type) {
         case types.SET_EXPRESSION:
-            let expression = state.expression + action.payload
+            let expression = setExpression(state, action);
             return {
                 ...state,
                 expression,
-                total: calculate(expression)
-            }
+                total: calculate(expression) || state.total
+            };
+        case types.CLEAR_EXPRESSION:
+            return {
+                ...state,
+                expression: "",
+                total: 0
+            };
+        case types.DELETE_LAST_EXPRESSION_ENTRY:
+            let exp = state.expression;
+            exp = exp
+                .split("")
+                .slice(0, exp.length - 1)
+                .join("");
+            return {
+                ...state,
+                expression: exp,
+                total: calculate(exp)
+            };
+        case types.EVALUATE_EXPRESSION:
+            return {
+                ...state,
+                expression: "",
+                total: calculate(state.expression) || state.expression || state.total
+            };
+        case types.SQUARE:
+            return {
+                ...state,
+                expression: "",
+                total: state.expression * state.expression
+            };
+        case types.SQUARE_ROOT:
+            return {
+                ...state,
+                expression: "",
+                total: Math.sqrt(state.expression)
+            };
         default:
-            return state
+            return state;
     }
-}
+};
